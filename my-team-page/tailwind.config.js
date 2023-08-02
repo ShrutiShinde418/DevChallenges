66603b2a200c49ffb127bccc782a9ba8b339b482
Shruti <shrutishinde736@gmail.com>
2023-08-02 20:24:55 +0530

    my team page added

---
+++ my-team-page/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        gray: "#a9a9a9",
+      },
+      fontFamily: {
+        montserrat: "Montserrat",
+        poppins: "Poppins",
+        serif: "PT Serif",
+      },
+    },
+  },
+  plugins: [],
+};
